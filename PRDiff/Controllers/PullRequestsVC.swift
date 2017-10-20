61d98be70f6b1de98de32c580ecf28ae4d39656d
Steve Wight <stevendouglaswight@gmail.com>
2017-10-20 11:14:25 -0700

    remove storyboard, launch from appDelegate, project structure

---
+++ PRDiff/Controllers/PullRequestsVC.swift
@@ -0,0 +1,19 @@
+//
+//  PullRequestsVC.swift
+//  PRDiff
+//
+//  Created by Steve on 10/20/17.
+//  Copyright © 2017 Steve Wight. All rights reserved.
+//
+
+import UIKit
+
+class PullRequestsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Pull Requests"
+    }
+
+}
+
