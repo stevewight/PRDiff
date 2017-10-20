5768689770bed755d74938502c175f9b873d3689
Steve Wight <stevendouglaswight@gmail.com>
2017-10-20 14:59:06 -0700

    setUp pullRequestVC tableView delegates

--- PRDiff/Controllers/PullRequestsVC.swift
+++ PRDiff/Controllers/PullRequestsVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class PullRequestsVC: UIViewController {
+class PullRequestsVC: UITableViewController {
     
     let githubAPI = GithubAPI()
     let repo = Repo(name: "Alamofire", owner: "Alamofire")
@@ -17,7 +17,15 @@ class PullRequestsVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Pull Requests"
+        registerCell()
         loadPullRequests()
+    }
+    
+    private func registerCell() {
+        tableView.register(
+            PullRequestCell.self,
+            forCellReuseIdentifier: "PullRequestCell"
+        )
     }
     
     private func loadPullRequests() {
@@ -32,12 +40,42 @@ class PullRequestsVC: UIViewController {
     }
     
     private func updateUI() {
-        print("updating UI")
+        // Make sure UI changes are on Main Queue
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
     }
     
     private func presentErrorMsg(_ errorMsg:String) {
         print("presenting error msg: \(errorMsg)")
     }
 
+}
+
+// MARK: UITableViewDelegate and UITableViewDataSource Methods
+
+extension PullRequestsVC {
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: "PullRequestCell", for: indexPath
+        ) as! PullRequestCell
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pullRequests.count
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 140.0
+    }
+    
 }
 
---
+++ PRDiff/Views/PullRequestCell.swift
@@ -0,0 +1,15 @@
+//
+//  PullRequestCell.swift
+//  PRDiff
+//
+//  Created by Steve on 10/20/17.
+//  Copyright © 2017 Steve Wight. All rights reserved.
+//
+
+import UIKit
+
+class PullRequestCell: UITableViewCell {
+
+
+
+}
